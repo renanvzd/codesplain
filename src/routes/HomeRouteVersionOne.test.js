0a431185a5763f195b839eaadab3f611c8a093f7
Renan Veronez Drechsler <renanvz@outlook.com>
2023-11-09 19:13:33 +0100

    createServer(handlerConfig)

--- src/routes/HomeRouteVersionOne.test.js
+++ src/routes/HomeRouteVersionOne.test.js
@@ -1,32 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
-import { createServer } from '../test/server';
 
-createServer([
-  {
-    path: '/api/repositories',
-    // method: 'get',
-    res: (req) => {
-      const language = req.url.searchParams.get('q').split('language:')[1];
-      return {
-        items: [
-          { id: 1, full_name: `${language}_one` },
-          { id: 2, full_name: `${language}_two` }
-        ]
-      }
-    }
-  },
-  // {
-  //   path: '/api/repositories',
-  //   method: 'post',
-  //   res: (req, res, ctx) => {
-  //     return {
-  //       items: [{}, {}, {}]
-  //     }
-  //   }
-  // }
-])
+const handlers = [
+  rest.get('/api/repositories', (req, res, ctx) => {
+    // const query = req.url.searchParams.get('q');
+    // console.log(query);
+
+    const language = req.url.searchParams.get('q').split('language:')[1];
+    console.log(language);
+
+    return res(ctx.json({
+      items: [
+        { id: 1, full_name: `${language}_one` },
+        { id: 2, full_name: `${language}_two` }
+      ]
+    }))
+  })
+]
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  server.resetHandlers();
+})
+
+afterAll(() => {
+  server.close();
+})
 
 test('renders two links for each language', async () => {
   render(
